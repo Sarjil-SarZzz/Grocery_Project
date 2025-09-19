83277baa2c116a6d66a002d9a0a95ec449611286
Sarjil-SarZzz <Sarjilhasan1234@gmail.com>
2025-09-19 20:16:14 +0600

    added fronted

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -10,12 +10,13 @@ export const AppContextProvider = ({ children }) => {
 
     const currency =import.meta.VITE_CURRENCY;
     const navigate = useNavigate();
-    const [user, setUser] = useState(true)
+    const [user, setUser] = useState(false)
     const [isSeller, setIsSeller] = useState(false)
     const [showUserLogin,setShowUserLogin] = useState(false)
     const [products,setProducts] = useState([])
 
     const [cartItems,setCartItems]=useState({})
+    const [searchQuery,setSearchQuery]=useState({})
 
     const fetchProducts = async ()=>{
         setProducts(dummyProducts)
@@ -53,12 +54,32 @@ export const AppContextProvider = ({ children }) => {
         setCartItems(cartData)
     }
 
+    // Get Cart Item Count
+    const getCartCount = ()=>{
+        let totalCount = 0;
+        for(const item in cartItems){
+            totalCount +=cartItems[item];
+        }
+        return totalCount;
+    }
+    // Get Cart Total Amount
+    const getCartAmount = ()=>{
+        let totalAmount = 0;
+        for(const items in cartItems){
+            let itemInfo = products.find((products)=>products._id === items);
+            if(cartItems[items] > 0){
+                totalAmount += itemInfo.offerPrice * cartItems[items]
+            }
+            return Math.floor(totalAmount * 100)/100;
+        }
+    }
+
 
     useEffect(()=>{
         fetchProducts()
     },[])
 
-    const value = { navigate, user, setUser, setIsSeller, isSeller,showUserLogin,setShowUserLogin,products, currency, addToCart, removeFromCart, updateCartItem,cartItems }
+    const value = { navigate, user, setUser, setIsSeller, isSeller,showUserLogin,setShowUserLogin,products, currency, addToCart, removeFromCart, updateCartItem,cartItems, searchQuery, setSearchQuery,getCartAmount, getCartCount }
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>
